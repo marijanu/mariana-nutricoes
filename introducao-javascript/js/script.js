2fcc167e992235fd1bc3f08ad8a6265939fb57f4
marijanu <130366468+marijanu@users.noreply.github.com>
2023-09-11 08:51:55 -0300

    Add files via upload

---
+++ introducao-javascript/js/script.js
@@ -0,0 +1,13 @@
+console.log("O arquivo separado está funcionando");
+
+//----------Daqui para baixo começa o código 
+
+//alteração do título 
+//console.log(document);
+//cria a variável titulo e armazena o conteudo da classe
+let titulo = document.querySelector(".titulo");
+//console.log(titulo);
+//atribuição de um novo valor na veriável titulo 
+titulo.textContent = "Fake Natty Nutrição!";
+
+//alteração da tabela
